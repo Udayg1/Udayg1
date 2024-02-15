b154d0f6a35204df64a27f1118d31a40e722906c
Udayg1 <110248880+Udayg1@users.noreply.github.com>
2024-02-15 20:18:07 +0530

    Update prime number checker.py.c

--- prime number checker.py.c
+++ prime number checker.py.c
@@ -1,7 +1,7 @@
-num= 79
-for i in range(2,num):
-  if num % i==0:
-   print(i,"times" ,num//i, "is" ,num)
+num= int(input("enter number:"))
+for k in range(2,num):
+  if num % k==0:
+   print(k,"times" ,num//k, "is" ,num)
    break
 else:
    print('the number is prime')
